7e39aa980fa4a4b5546ff5f0b6727f90b8e7fb7c
PCDELL\Mario <mario.leiva.al@gmail.com>
2017-11-29 18:21:04 -0300

    Limpieza de comentarios

--- src/EjerP/hola_mundo.java
+++ src/EjerP/hola_mundo.java
@@ -3,11 +3,11 @@ package EjerP;
 public class hola_mundo {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("hola git :) con Mario");
-=======
+
 		System.out.println("hola git :) con Sebastian");
->>>>>>> e141818d22820c1342182bb35e10c3c25275e9f3
+
 
 	}
 
