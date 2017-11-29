e39de8c78426494b3fd20db67f6a79a5a8dfd1e6
SEBA\seba <seba_nl_10@hotmail.com>
2017-11-29 18:36:40 -0300

    version anterior

--- src/EjerP/hola_mundo.java
+++ src/EjerP/hola_mundo.java
@@ -3,10 +3,14 @@ package EjerP;
 public class hola_mundo {
 
 	public static void main(String[] args) {
+<<<<<<< HEAD
 
 		System.out.println("hola git :) con Mario y Sebastian");
 
 
+=======
+		System.out.println("hola git :)");
+>>>>>>> parent of 08c115a... Mis cambios
 
 	}
 
